b8a1a6c2ac989d137a50e11664d8c89e4f9a192b
Ecoham <59919892+Ecoham@users.noreply.github.com>
2020-12-20 16:49:30 +0500

    Default Settings

--- src/index.js
+++ src/index.js
@@ -1,16 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+
+ReactDOM.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
+    document.getElementById('root')
+);
+
+
+serviceWorker.unregister();
+
+
+
+
+
+
+
 
 
 //Excercises
-function MediaCard(props) {
-return <div>
-    <h2> {props.Title}</h2>
-        <p> We are Learning <strong>{props.Body}</strong></p>
-        <img src={props.imageUrl} /> 
-</div>
-}
-ReactDOM.render(<MediaCard Title="REACT" Body="React" imageUrl='https://servmask.com/img/products/url.png'/>,document.querySelector('#root'));
+// function MediaCard(props) {
+// return <div>
+//     <h2> {props.Title}</h2>
+//         <p> We are Learning <strong>{props.Body}</strong></p>
+//         <img src={props.imageUrl} /> 
+// </div>
+// }
+// ReactDOM.render(<MediaCard Title="REACT" Body="React" imageUrl='https://servmask.com/img/products/url.png'/>,document.querySelector('#root'));
 
 // function Gate(props){
 // return <div>
