0cc9c1395826dc7b91855fbdb954b0152b21ad6b
Ecoham <59919892+Ecoham@users.noreply.github.com>
2020-12-25 11:32:07 +0500

    Learned useState
    After stateless components, I learned useState functionalities on react-app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
-
-import './App.css';
-
+import React,{useState} from 'react';
 function App() {
+  const [count, setCount] = useState(()=>{
+    console.log('run function') //if require to add computational functionalities
+    return 4                   // we use function in a useState
+  })
+  const [theme, setTheme] = useState('blue')
+  function decrementcount (){
+    setCount(preCount => preCount - 1)
+    setTheme('blue')
+  }
+  function incrementcount (){
+    setCount(preCount => preCount + 1)
+    setTheme('red')
+  }
   return (
-    <div className="App">
-      
+    <div >
+      <button onClick = {incrementcount}>+</button>
+      <span>{count}</span>
+      <span>{theme}</span>
+      <button onClick = {decrementcount}>-</button>
     </div>
   );
 }
